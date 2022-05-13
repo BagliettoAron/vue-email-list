65f27f3410d645c93202313d869451d069ad962a
@BagliettoAron <aronbaglietto88@gmail.com>
2022-05-13 21:26:40 +0200

    Add: printed 10 mail using Axios and Vue

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+const app= new Vue({
+
+    el:'#root',
+    data: {
+        newMail: [],
+        
+        
+        
+        
+    },
+    
+    created() {
+        
+        for (let i = 0; i < 10; i++) {
+            
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then ((resp) => {
+                console.log(resp.data.response);
+                this.newMail.push(resp.data.response);
+            })
+        }
+        
+    },
+});
+
+
+
+
